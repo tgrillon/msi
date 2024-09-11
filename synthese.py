cb5b7790ccee2f3e025c28a2e5ccf5c53a91cebd
Théo Grillon <theo.grillon@etu.univ-lyon1.fr>
2024-09-11 19:52:18 +0200

    choix du patch + première version du calcul de distance

--- synthese.py
+++ synthese.py
@@ -2,27 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np 
 from PIL import Image 
 
-# Load source image 
-src= Image.open("data/text2.png")
-srcArray= np.array(src) 
-
-# Define destination image
-dst= Image.new(size=(src.size[0]*2, src.size[1]*2), mode="RGB") 
-dstArr= np.array(dst)
-
 # Define patch dimensions
 dim= 23  
 
+# Load source image 
+src= Image.open("data/text2.png")
+srcBis= Image.new(size=(src.size[0]+dim-1, src.size[1]+dim-1), mode="RGB") 
+
+srcArr= np.array(srcBis)
+srcArr[dim//2:dim//2+src.size[1], dim//2:dim//2+src.size[0]]= np.array(src) 
+
+# Define destination image
+h, w= 250, 250
+dst= Image.new(size=(w+dim-1, h+dim-1), mode="RGB") 
+dstArr= np.array(dst)
+
 # Initisalize I_smp
-x= np.random.randint(0, srcArray.shape[0]-dim-1)
-y= np.random.randint(0, srcArray.shape[1]-dim-1)
-smp= srcArray[x:x+dim, y:y+dim]
+x= np.random.randint(dim//2, srcArr.shape[0]-dim-1-dim//2)
+y= np.random.randint(dim//2, srcArr.shape[1]-dim-1-dim//2)
+smp= srcArr[x:x+dim, y:y+dim]
 
 # Initialize I with I_smp
-x= np.random.randint(0, dstArr.shape[0]-dim-1)
-y= np.random.randint(0, dstArr.shape[1]-dim-1)
-# dstArr[x:x+dim, y:y+dim]= smp
-dstArr[dim//4:dim//4+dim, dim+dim//4:2*dim+dim//4]= smp
+x= np.random.randint(dim//2, dstArr.shape[0]-dim-1-dim//2)
+y= np.random.randint(dim//2, dstArr.shape[1]-dim-1-dim//2)
+
+dstArr[x:x+dim, y:y+dim]= smp
 
 # plt.imshow(dstArr)
 # plt.show()
@@ -54,24 +58,16 @@ def binaryArray(arr) -> np.array:
         bin[x, y]= 0
   return bin
 
-def extractSquareArroundP(arr, p, dim) -> [np.array, np.uint64, np.uint64, np.uint64, np.uint64]:
-  minx= max((0, p[0]-dim//2))
-  maxx= min((arr.shape[0], p[0]+dim//2))
-  miny= max((0, p[1]-dim//2))
-  maxy= min((arr.shape[1], p[1]+dim//2))
-  return arr[p[0]-dim//2:p[0]+dim//2, p[1]-dim//2:p[1]+dim//2], minx, maxx, miny, maxy
-
 def numberOfNeighbors(arr, edg, dim) -> [np.array, np.uint64] :
   ret= np.empty(shape=(0,1), dtype=np.float64)
+  msk= np.ones((dim, dim, 1), dtype=np.uint8)
   maxN= 0
   for e in edg:
-    smp, minx, maxx, miny, maxy= extractSquareArroundP(arr, e, dim)
-    msk= np.ones((maxx-minx, maxy-miny, 1), dtype=np.uint8)
-
-    # Compute and store the number of neighbors filled 
+    smp= arr[e[0]-dim//2:e[0]+dim//2+1, e[1]-dim//2:e[1]+dim//2+1]
+    # Compute and store the number of neighbors filled  
     cnv= np.convolve(smp.flatten(), msk.flatten(), mode="valid")
     ret= np.append(ret, np.array(cnv))
-    
+
     # Update max value 
     maxN= max(maxN, cnv)
     
@@ -82,48 +78,70 @@ bin= binaryArray(dstArr)
 
 ngh, maxN= numberOfNeighbors(bin, edg, dim)
 
-s= 0.90
+s= 0.95
 ths= s*maxN
 
 
 # Get the index of pixels with most neighbors 
 ids= np.where(ngh > ths)  
 ids= ids[0]
-print(ngh)
-print(ids)
 
-ridx= np.random.randint(0, ids.shape[0]-1)
-print(edg[ridx])
-patch= extractSquareArroundP(dstArr, edg[ridx], dim)[0]
+i= np.random.randint(0, ids.shape[0]-1)
+ptch= dstArr[edg[ids[i]][0]-dim//2:edg[ids[i]][0]+dim//2+1, edg[ids[i]][1]-dim//2:edg[ids[i]][1]+dim//2+1] 
 
-# Number of patches
-num_patches = len(ids)
+# # Number of patches
+# num_patches = len(ids)
 
-# Determine grid size (e.g., 2x2 grid for 4 patches)
-grid_size = int(np.ceil(np.sqrt(num_patches)))
+# # Determine grid size (e.g., 2x2 grid for 4 patches)
+# grid_size = int(np.ceil(np.sqrt(num_patches)))
 
-# Create subplots
-fig, axes = plt.subplots(grid_size, grid_size, figsize=(10, 10))
+# # Create subplots
+# fig, axes = plt.subplots(grid_size, grid_size, figsize=(10, 10))
 
-# Flatten the axes array for easy iteration
+# # Flatten the axes array for easy iteration
+# axes = axes.flatten()
+
+# # Plot each patch
+# for i in range(num_patches):
+#   ptch= dstArr[edg[ids[i]][0]-dim//2:edg[ids[i]][0]+dim//2+1, edg[ids[i]][1]-dim//2:edg[ids[i]][1]+dim//2+1]
+#   axes[i].imshow(ptch, cmap='gray', interpolation='none')
+#   axes[i].axis('off')  # Hide axes
+
+# plt.tight_layout()
+# plt.show()
+
+# plt.imshow(ptch)
+# plt.show()
+
+def distance2(p, q) -> np.uint64:
+  return (p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])+(p[2]-q[2])*(p[2]-q[2])
+
+def patchDistance(ptchA, ptchB) -> np.uint64:
+  dist= 0  
+  dim= ptchA.shape[0]
+  for x in range(dim):
+    for y in range(dim):
+      dist= dist+distance2(ptchA[x, y], ptchB[x, y])
+  return dist
+
+dist= np.zeros(shape=(srcArr.shape[0], srcArr.shape[1], 1), dtype=np.uint64)
+for x in range(dim//2, srcArr.shape[0]-dim//2): 
+  for y in range(dim//2, srcArr.shape[1]-dim//2): 
+    ptchA= srcArr[x-dim//2:x+dim//2+1, y-dim//2:y+dim//2+1]
+    dist[x, y]= patchDistance(ptchA, ptch)
+
+fig, axes = plt.subplots(1, 3, figsize=(10, 10))
 axes = axes.flatten()
 
-# Plot each patch
-for i in range(num_patches):
-  patch= extractSquareArroundP(dstArr, edg[ids[i]], dim)[0]
-  axes[i].imshow(patch, cmap='gray', interpolation='none')
-  axes[i].axis('off')  # Hide axes
+axes[0].imshow(ptch, cmap='gray', interpolation='none')
+axes[0].axis('off')  # Hide axes
+
+axes[1].imshow(srcArr, cmap='gray', interpolation='none')
+axes[1].axis('off')  # Hide axes
+
+axes[2].imshow(dist, cmap='gray', interpolation='none')
+axes[2].axis('off')  # Hide axes
 
 plt.tight_layout()
 plt.show()
-
-plt.imshow(dstArr)
-plt.show()
-
-
-# plt.imshow(binaryArray(dstArr))
-# plt.show()
-
-# print(binaryArray(dstArr))
-
 
