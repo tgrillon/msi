8ef12a76fb433771f8abb107ab5c6cd1255f496d
tgrillon <tgrillon@friska.home>
2024-09-17 09:02:03 +0200

    update

--- synthese.py
+++ synthese.py
@@ -7,69 +7,73 @@ from PIL import Image
 
 #================INITIALIZATION=================#
 
-# Define patch dimensions
-dim= 5
+# Define patch patch_sizes
+patch_size= 5
 
 # Define epsilon error
-eps= 0.2
+epsilon= 0.2
 
 # Load source image 
-src= Image.open("data/text2.png")
-srcBis= Image.new(size=(src.size[0]+dim-1, src.size[1]+dim-1), mode="RGB") 
-
-srcArr= np.array(srcBis)
-srcArr[dim//2:dim//2+src.size[1], dim//2:dim//2+src.size[0]]= np.array(src) 
+source= Image.open("data/text2.png")
+source_array= np.array(source)
 
 # Define destination image
-h, w= 250, 250
-dst= Image.new(size=(w+dim-1, h+dim-1), mode="RGB") 
-dstArr= np.array(dst)
+height, width= 64, 64
+destination_array= np.zeros(shape=(width, height, 3), dtype=np.uint8) 
 
 # Initisalize I_smp
-x= np.random.randint(dim//2, srcArr.shape[0]-dim-1-dim//2)
-y= np.random.randint(dim//2, srcArr.shape[1]-dim-1-dim//2)
-smp= srcArr[x:x+dim, y:y+dim]
+x= np.random.randint(0, source_array.shape[0]-patch_size)
+y= np.random.randint(0, source_array.shape[1]-patch_size)
+patch_sample= source_array[x:x+patch_size, y:y+patch_size]
 
 # Initialize I with I_smp
-x= np.random.randint(dim//2, dstArr.shape[0]-dim-1-dim//2)
-y= np.random.randint(dim//2, dstArr.shape[1]-dim-1-dim//2)
+x= np.random.randint(0, destination_array.shape[0]-patch_size)
+y= np.random.randint(0, destination_array.shape[1]-patch_size)
 
-dstArr[x:x+dim, y:y+dim]= smp
+destination_array[x:x+patch_size, y:y+patch_size]= patch_sample
+
 #===============================================#
-#TODO Tant qu'il reste des pixels vides dans l'image destination (dst)
+#TODO Tant qu'il reste des pixels vides dans l'image destination (destination)
 #TODO   Faire l'étape 1 
 #TODO   Tant qu'il reste des pixels vides dans edg
 #TODO     Faire l'étape 2 
 #===============================================#
 
-t0 = time.time()
+elapsed_time_0 = time.time()
 
-edg= utils.getEdges(dstArr)
+binary_array = np.any(destination_array != [0, 0, 0], axis=-1).astype(int)
+edges= utils.find_unfilled_with_neighbors(binary_array)
 
-bin = np.any(dstArr != [0, 0, 0], axis=-1).astype(int)
+while len(edges) > 0: 
+  neighbors, max_value= utils.compute_number_of_neighbors(binary_array, edges, patch_size)
+  print(neighbors)
+  print("max_value: ", max_value)
+  break
 
-ngh, maxN= utils.numberOfNeighbors(bin, edg, dim)
+elapsed_time_1 = time.time()
+print("Step 1: ", elapsed_time_1-elapsed_time_0)
 
-t1 = time.time()
-print("Step 1: ", t1-t0)
-t0 = time.time()
 
-s= 0.95
-ths= s*maxN
+# ngh, maxN= utils.numberOfNeighbors(binary_array, edg, patch_size)
 
-# Get the index of pixels with most neighbors 
-ids= np.where(ngh > ths)  
-ids= ids[0]
+# elapsed_time_0 = time.time()
 
-i= np.random.randint(0, ids.shape[0]-1)
-ptch= dstArr[edg[ids[i]][0]-dim//2:edg[ids[i]][0]+dim//2+1, edg[ids[i]][1]-dim//2:edg[ids[i]][1]+dim//2+1] 
-ptch= ptch.astype(np.float64)
+# s= 0.95
+# ths= s*maxN
 
-color, dist= utils.getMatchingColor(srcArr, ptch, dim, eps)
-print(color)
+# # Get the index of pixels with most neighbors 
+# ids= np.where(ngh > ths)  
+# ids= ids[0]
 
-t1 = time.time()
-print("Step 2: ", t1-t0)
+# i= np.random.randint(0, ids.shape[0]-1)
+# ptch= destination_array[edg[ids[i]][0]-patch_size//2:edg[ids[i]][0]+patch_size//2+1, edg[ids[i]][1]-patch_size//2:edg[ids[i]][1]+patch_size//2+1] 
+# ptch= ptch.astype(np.float64)
+
+# color, dist= utils.getMatchingColor(source_array, ptch, patch_size, epsilon)
+# print(color)
+
+# elapsed_time_1 = time.time()
+# print("Step 2: ", elapsed_time_1-elapsed_time_0)
 
 # fig, axes = plt.subplots(1, 3, figsize=(10, 10))
 # axes = axes.flatten()
@@ -77,7 +81,7 @@ print("Step 2: ", t1-t0)
 # axes[0].imshow(ptch.astype(np.uint8), cmap='gray', interpolation='none')
 # axes[0].axis('off')  # Hide axes
 
-# axes[1].imshow(srcArr, cmap='gray', interpolation='none')
+# axes[1].imshow(source_array, cmap='gray', interpolation='none')
 # axes[1].axis('off')  # Hide axes
 
 # axes[2].imshow(dist, cmap='gray', interpolation='none')
--- utils.py
+++ utils.py
@@ -1,60 +1,93 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+from scipy.ndimage import convolve
 
-def filled(p) -> bool : 
-  return p[0]>0 or p[1]>0 or p[2]>0  
+def find_unfilled_with_neighbors(binary_array):
+  shifted_up= np.roll(binary_array, shift=-1, axis=0)
+  shifted_down= np.roll(binary_array, shift=1, axis=0)
+  shifted_left= np.roll(binary_array, shift=-1, axis=1)
+  shifted_right= np.roll(binary_array, shift=1, axis=1)
 
-def hasANeighbor(arr, x, y) -> bool:
-  return x+1 < arr.shape[0] and filled(arr[x+1, y]) or x-1 >= 0 and filled(arr[x-1, y]) or y+1 < arr.shape[1] and filled(arr[x, y+1]) or y-1 >= 0 and filled(arr[x, y-1])
+  combined_mask= shifted_up | shifted_down | shifted_left | shifted_right
+  # print(combined_mask)
+  unfilled_mask= binary_array == 0
+  result_mask= unfilled_mask & combined_mask
 
-def getEdges(arr) -> np.array:
-  edg= np.empty((0,2), dtype=np.int64)
-  for x in range(arr.shape[0]):
-    for y in range(arr.shape[1]):
-      if not filled(arr[x, y]) and hasANeighbor(arr, x, y):
-        edg= np.append(edg, np.array([[x, y]]), axis=0)
-  return edg
+  coordinates= np.argwhere(result_mask)
+  
+  result_coordinates= [tuple(coord[::-1]) for coord in coordinates]
 
-def numberOfNeighbors(arr, edg, dim) -> [np.array, np.uint64] :
-  ret= np.empty(shape=(0,1), dtype=np.float64)
-  msk= np.ones((dim, dim, 1), dtype=np.uint64)
-  maxN= 0
-  for e in edg:
-    smp= arr[e[0]-dim//2:e[0]+dim//2+1, e[1]-dim//2:e[1]+dim//2+1]
-    # Compute and store the number of neighbors filled  
-    cnv= np.convolve(smp.flatten(), msk.flatten(), mode="valid")
-    ret= np.append(ret, np.array(cnv))
+  return result_coordinates
 
-    # Update max value 
-    maxN= max(maxN, cnv)
+def compute_number_of_neighbors(binary_array, candidates, patch_size) -> [np.array, np.uint64] :
+  neighbors_count= np.zeros(len(candidates), dtype=np.uint64)
+  mask= np.ones((patch_size, patch_size), dtype=np.uint64)
+  max_value= 0
+  conv_array= convolve(binary_array, mask)
+  for i, (x, y) in enumerate(candidates):
+    neighbors_count[i]= conv_array[x, y] 
+
+  max_value= np.max(neighbors_count)
+  return [neighbors_count, max_value]
+  # pad_size= patch_size//2 
+  # extended_array= np.pad(binary_array, pad_width=pad_size, mode='constant', constant_values=0)
+  # mask= np.ones((patch_size, patch_size), dtype=np.uint64)
+  # neighbors_count= np.zeros(len(candidates), dtype=np.uint64)
+  # conv_array= np.convolve(binary_array.flatten(), mask.flatten(), mode="same")
+  # print(conv_array) 
+  # for idx, (x, y) in enumerate(candidates):
+  #   patch= extended_array[x-pad_size:x+pad_size+1, y-pad_size:y+pad_size+1] 
+  #   neighbors_count[idx]= np.convolve(patch.flatten(), mask.flatten(), mode="valid")
+  # max_value = np.max(neighbors_count)
+  # return neighbors_count, max_value
+
+def compute_patch_distance(patch_a, patch_b, min_error, epsilon) -> np.float64:
+    squared_diff= (patch_a - patch_b) ** 2
     
-  return [ret, maxN]
+    cumsum_squared_diff= np.cumsum(squared_diff)
+    
+    for i in range(cumsum_squared_diff.size):
+        if cumsum_squared_diff[i] > (1 + epsilon) * min_error:
+            return cumsum_squared_diff[i]
+    
+    return cumsum_squared_diff[-1]
 
-def distance2(p, q) -> np.float64:
-  return (p[0]-q[0])*(p[0]-q[0])+(p[1]-q[1])*(p[1]-q[1])+(p[2]-q[2])*(p[2]-q[2])
+def find_matching_color(source_array, patch_sample, patch_size, epsilon) -> [np.array, np.array]: 
+  min_error= float("inf")
+  patch_distances= np.zeros(shape=(source_array.shape[0], source_array.shape[1], 1), dtype=np.float64)
+  for x in range(patch_size//2, source_array.shape[0]-patch_size//2): 
+    for y in range(patch_size//2, source_array.shape[1]-patch_size//2): 
+      patch_source= source_array[x-patch_size//2:x+patch_size//2+1, y-patch_size//2:y+patch_size//2+1]
+      patch_source= patch_source.astype(np.float64)
+      patch_distances[x, y]= compute_patch_distance(patch_source, patch_sample, min_error, epsilon)
+      min_error= min(min_error, patch_distances[x, y])
 
-def patchDistance(ptchA, ptchB, minErr, eps) -> np.float64:
-  dist= 0  
-  dim= ptchA.shape[0]
-  for x in range(dim):
-    for y in range(dim):
-      dist= dist+distance2(ptchA[x, y], ptchB[x, y])
-      if dist>(1+eps)*minErr: return dist
-  return dist
-
-def getMatchingColor(srcArr, ptch, dim, eps) -> [np.array, np.array]: 
-  minErr= float("inf")
-  dist= np.zeros(shape=(srcArr.shape[0], srcArr.shape[1], 1), dtype=np.float64)
-  for x in range(dim//2, srcArr.shape[0]-dim//2): 
-    for y in range(dim//2, srcArr.shape[1]-dim//2): 
-      ptchA= srcArr[x-dim//2:x+dim//2+1, y-dim//2:y+dim//2+1]
-      ptchA= ptchA.astype(np.float64)
-      dist[x, y]= patchDistance(ptchA, ptch, minErr, eps)
-      minErr= min(minErr, dist[x, y])
-
-  indices = np.where((dist[dim//2:srcArr.shape[0]-dim//2, dim//2:srcArr.shape[1]-dim//2, 0] <= (eps + 1) * minErr) & (dist[dim//2:srcArr.shape[0]-dim//2, dim//2:srcArr.shape[1]-dim//2, 0] > 0))
-  x_coords, y_coords = indices
-  candidates = list(zip(x_coords, y_coords))
-  print(candidates)
+  candidates_indices= np.where((patch_distances[patch_size//2:source_array.shape[0]-patch_size//2, patch_size//2:source_array.shape[1]-patch_size//2, 0] <= (epsilon + 1) * min_error) & (patch_distances[patch_size//2:source_array.shape[0]-patch_size//2, patch_size//2:source_array.shape[1]-patch_size//2, 0] > 0))
+  x_coords, y_coords= candidates_indices
+  candidates= list(zip(x_coords, y_coords))
 
   i= np.random.randint(0, len(candidates))
-  return [srcArr[candidates[i][0], candidates[i][1]], dist]
+  return [source_array[candidates[i][0], candidates[i][1]], patch_distances]
+
+
+# arr= np.array([
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#   [0, 0, 0, 1, 1, 1, 0, 0, 0], 
+#   [0, 0, 0, 1, 1, 1, 0, 0, 0], 
+#   [0, 0, 0, 1, 1, 1, 0, 0, 0], 
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+#   [0, 0, 0, 0, 0, 0, 0, 0, 0], 
+# ])
+
+# mask= np.ones((5, 5), dtype=np.uint64)
+# extended_arr= np.pad(arr, pad_width=2, mode='constant', constant_values=0)
+# conv_array= convolve(extended_arr, mask)
+# conv_array= np.convolve(extended_arr.flatten(), mask.flatten(), mode="valid")
+# conv_array= np.reshape(conv_array, (extended_arr.shape[0], extended_arr.shape[1]))
+# print(conv_array)
+
+# print(np.pad(arr, pad_width=2, mode='constant', constant_values=0))
+# print(find_unfilled_with_neighbors(arr))
